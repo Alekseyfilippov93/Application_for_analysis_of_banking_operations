f7571af1d3664bd834ee434449a6f2fc9b6f0fef
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-04-19 15:37:15 +0700

    Обновление кода. Перенос функций в модуль utils

--- src/services.py
+++ src/services.py
@@ -1,28 +1,15 @@
-import pandas as pd
-from src.utils import PATH_TO_FILE
+from collections import defaultdict
 
 
-def analize_chashbek(data_file: str, year: int, month: int):
+def analyze_cashback(transactions, year, month):
     """
     Анализ выгодности категорий повышенного кешбэка.
     Возвращает данные за указанный месяц и год в виде списка словарей.
     """
-    print(data_file)
-    try:
-        df = pd.read_excel(data_file)
-    except Exception as e:
-        return {}  # Возвращаем пустой DataFrame
-
-    # Преобразование даты
-    df["Дата операции"] = pd.to_datetime(df["Дата операции"].str.strip(), dayfirst=True, errors="coerce")
-
-    # Фильтр по году и месяцу
-    mask = (df["Дата операции"].dt.year == year) & (df["Дата операции"].dt.month == month)
-    filter_date = df[mask]
-
-    return filter_date.to_dict("dict")
-
-
-if __name__ == "__main__":
-    result = analize_chashbek(PATH_TO_FILE, 2019, 12)
-    print(result)
+    filtered = filter(lambda x: x["date"].year == year and x["date"].month == month, transactions)
+    cashback_summary = defaultdict(float)
+    for txn in filtered:
+        category = txn["category"]
+        cashback = txn["cashback"]
+        cashback_summary[category] += cashback
+    return dict(cashback_summary)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 from datetime import datetime
-import os
 from pathlib import Path
+import json
+import os
+from typing import List, Any
+import pandas as pd
+from dotenv import load_dotenv
+import yfinance as yf
+import requests
+
+load_dotenv()
 
 PATH_TO_FILE = Path(__file__).parent.parent / "data" / "operations.xlsx"
 
@@ -39,3 +47,77 @@ def get_date_time(date_time: str, date_format: str = "%Y-%m-%d %H:%M:%S"):
     end_date_str = datetime.strftime(end_date, "%d.%m.%Y %H:%M:%S")
     period_date = [start_day_str, end_date_str]
     return period_date
+
+
+def main(date):
+    """
+    Функция для страницы «Главная» принимает на вход строку с датой и временем в формате
+    YYYY-MM-DD HH:MM:SS и возвращающую JSON-ответ
+    """
+    greeting = get_time_for_greeting()
+    time_period: str = get_date_time(date)
+    data = {"greeting": greeting, "time_period": time_period}
+    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+    return json_data
+
+
+def calculate_total_expenses(df, card_number=None, category=None, cashback=False):
+    """
+    Считает общую сумму расходов из Excel-файла.
+    """
+    # Загружаем Excel-файл
+    df = pd.read_excel(PATH_TO_FILE)
+
+    # Фильтр: оставляем только завершённые операции ("OK") и отрицательные суммы (расходы)
+    df_expenses = df[(df["Статус"] == "OK") & (df["Сумма операции"] < 0)].copy()
+
+    # Если указан номер карты, фильтруем по нему
+    if card_number:
+        df_expenses = df_expenses[df_expenses["Номер карты"] == card_number]
+    if category:
+        df_expenses = df_expenses[df_expenses["Категория"] == category]
+
+    # Сумма расходов (без кэшбэка)
+    total_expenses = df_expenses["Сумма операции"].abs().sum()
+
+    # Если нужно учесть кэшбэк
+    if cashback:
+        total_cashback = df_expenses["Бонусы (включая кэшбэк)"].sum()
+        net_expenses = total_expenses - total_cashback
+        return round(net_expenses, 2)
+
+
+def top_transactions_5(df: List[dict]) -> List[dict]:
+    """Функция возвращает топ 5 транзакций"""
+    df = pd.read_excel(PATH_TO_FILE)
+    df.sort(key=lambda x: x["Сумма операции"], reverse=True)
+    return df[:5]
+
+
+API_KEY = os.getenv("API_KEY")
+
+
+def get_cur_rate(currency: str) -> Any:
+    """Функция для получения курса валют."""
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}"
+    response = requests.get(url, headers={"apikey": API_KEY}, timeout=40)
+    response_data = json.loads(response.text)
+    return response_data["rates"]["RUB"]
+
+
+def get_stock_prices(symbols=["AAPL", "AMZN", "GOOG", "MSFT", "TSLA"]):
+    """
+    Получает текущие цены акций через Yahoo Finance
+    """
+    try:
+        data = yf.download(tickers=" ".join(symbols), period="1d", group_by="ticker", progress=False)
+
+        return {
+            "stock_prices": [
+                {"stock": symbol, "price": round(float(data[symbol]["Close"][-1]), 2)}
+                for symbol in symbols
+                if symbol in data and not data[symbol]["Close"].empty
+            ]
+        }
+    except Exception:
+        return {"stock_prices": []}
