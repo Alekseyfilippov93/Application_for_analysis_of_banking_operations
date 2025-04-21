4f378f5727c1f99c72b0f7a38dff3a95b7461a84
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-04-22 01:20:13 +0700

    upd services

--- src/services.py
+++ src/services.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import datetime
 
 
 def analyze_cashback(transactions, year, month):
@@ -6,10 +7,14 @@ def analyze_cashback(transactions, year, month):
     Анализ выгодности категорий повышенного кешбэка.
     Возвращает данные за указанный месяц и год в виде списка словарей.
     """
-    filtered = filter(lambda x: x["date"].year == year and x["date"].month == month, transactions)
+    filtered = filter(
+        lambda x: datetime.strptime(x["Дата операции"], "%d.%m.%Y %H:%M:%S").year == year
+                  and datetime.strptime(x["Дата операции"], "%d.%m.%Y %H:%M:%S").month == month,
+        transactions
+    )
     cashback_summary = defaultdict(float)
     for txn in filtered:
-        category = txn["category"]
-        cashback = txn["cashback"]
+        category = txn["Категория"]
+        cashback = txn["Кэшбэк"]
         cashback_summary[category] += cashback
     return dict(cashback_summary)
